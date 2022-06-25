e884ab87ad4e920fd2423ccefd5e7146aba0b4c1
Beauty <105244471+Bountukp20@users.noreply.github.com>
2022-06-25 01:26:26 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+function readMe(){
+  var show = document.getElementById("show");
+  show.innerText = "Hello, I am Ukpong Bounty Beauty, I am 154cm tall and I'm a Nigerian."
+  show.style.color = "darkred"
+}
